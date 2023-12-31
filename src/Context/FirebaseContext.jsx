d3001b31f2277484855a7eed5efc6e7b6a8aaa68
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2023-12-31 11:34:05 +0600

    create context functions for firebase operations

--- src/Context/FirebaseContext.jsx
+++ src/Context/FirebaseContext.jsx
@@ -1,18 +1,52 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth';
+import app from "../Firebase/firebase.config";
 
 
 const AuthContext = createContext(null);
-
-
+const FirebaseAuth = getAuth(app);
 
 const FirebaseContext = ({ children }) => {
     const [user,setUser]=useState()
     const [loader,setLoader]= useState(true)
 
+    const FirebaseLogin=(email,pass)=>{
+        setLoader(true);
+        return signInWithEmailAndPassword(FirebaseAuth,email,pass);
+    }
+    const FirebaseSignup=(email,pass)=>{
+        setLoader(true);
+        return createUserWithEmailAndPassword(FirebaseAuth,email,pass)
+    }
+    const FirebaseUpdateUserProfile=(val,name,photo)=>{
+        setLoader(true);
+        return updateProfile(val,{
+            displayName:name,
+            photoURL:photo
+        })
+    }
+    const FirebaseLogOutUser=()=>{
+        setLoader(true);
+        return signOut(FirebaseAuth);
+    }
+    useEffect(()=>{
+        const unSubscribe=(FirebaseAuth,currentUser=>{
+            setLoader(false);
+            setUser(currentUser);
+        })
+        return ()=>{
+            return unSubscribe()
+        }
+    },[])
+
 
     const authInfo = {
         user,
         loader,
+        FirebaseLogin,
+        FirebaseSignup,
+        FirebaseUpdateUserProfile,
+        FirebaseLogOutUser
     }
 
     return (
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChOvk3nyyN6Liw6GsKErtKcXZwJPgEIow",
+  authDomain: "phprac-e9d11.firebaseapp.com",
+  projectId: "phprac-e9d11",
+  storageBucket: "phprac-e9d11.appspot.com",
+  messagingSenderId: "894769508458",
+  appId: "1:894769508458:web:3b745c88151883ac9e9a13"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
